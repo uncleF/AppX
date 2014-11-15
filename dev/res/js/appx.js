2accc69641e59527d5fc3c63f59d57b4a1ed01b3
uncleF <uncle.funkay@gmail.com>
2014-11-15 11:27:19 +0300

    Project Update

--- dev/res/js/appx.js
+++ dev/res/js/appx.js
@@ -1,125 +1,77 @@
-function whichDevice() {
-	var USER_AGENT_STRING = navigator.userAgent.toLowerCase();
-	var MOBILE_LIST = new Array("iphone os 5", "ipad; cpu os 5", "iphone", "ipad", "android 2", "android", "blackberry", "palmos");
-	for (var DEVICE in MOBILE_LIST) {
-		if (USER_AGENT_STRING.indexOf(MOBILE_LIST[DEVICE])>=0) {
-			return MOBILE_LIST[DEVICE];
-		}
-	}
-}
-
-function whichTransitionEndEvent() {
-	var TRANSITION;
-	var ELEMENT = document.createElement('fakeelement');
-	var TRANSITIONS = {
-		"transition": "transitionend",
-		"OTransition": "oTransitionEnd",
-		"MozTransition": "transitionend",
-		"WebkitTransition": "webkitTransitionEnd"
-	};
-	for(TRANSITION in TRANSITIONS){
-		if(ELEMENT.style[TRANSITION] !== undefined) {
-			return TRANSITIONS[TRANSITION];
-		}
-	}
-}
-
-var transitionEndEvent = whichTransitionEndEvent();
-
-var App = Backbone.Model.extend({
-	defaults: {
-		state: "start"
-	}
-});
-
+var App = Backbone.Model.extend({ defaults: { state: "start" } });
 var app = new App();
-
 var AppView = Backbone.View.extend({
-	model: app,
-	id: "app",
-	screens: [],
-	initialize: function () {
-		this.render();
-		this.model.on("change:state", this.changeScreens, this);
-		this.createScreen();
-	},
-	render: function() {
-		$("body").prepend(this.$el);
-	},
-	events: {
-		"transitionend .screen-oldScreen, oTransitionEnd .screen-oldScreen, webkitTransitionEnd .screen-oldScreen": function() {
-			this.screens[0].unbind();
-			this.screens[0].remove();
-			this.screens.shift();
-			var afterScreenTransitionCall = this.afterScreenTransitionCalls[this.model.get("state") + "AfterCall"];
-			if (typeof afterScreenTransitionCall == "function") {
-				afterScreenTransitionCall();
-			}
-			$(".screen-newScreen").removeClass("screen-newScreen");
-			$("body").removeClass("is-screenTransition is-reverseTransition");
+		model: app,
+		id: "app",
+		screens: [],
+		initialize: function () {
+				this.render();
+				this.model.on("change:state", this.changeScreens, this);
+				this.createScreen();
 		},
-		"click .back": function(event) {
-			event.preventDefault();
-			$("body").addClass("is-reverseTransition");
-			history.back();
-		}
-	},
-	createScreen: function() {
-		var newScreen = new ScreenView();
-		if (this.screens.length > 0) {
-			newScreen.$el.addClass("screen-newScreen");
-		}
-		this.screens.push(newScreen);
-		this.$el.append(newScreen.$el);
-	},
-	changeScreens: function() {
-		this.createScreen();
-		var beforeScreenTransitionCall = this.beforeScreenTransitionCalls[this.model.get("state") + "BeforeCall"];
-		if (typeof beforeScreenTransitionCall == "function") {
-			beforeScreenTransitionCall();
-		}
-		this.screens[0].$el.addClass("screen-oldScreen");
-		setTimeout(function() {
-			$("body").addClass("is-screenTransition");
-		}, 10);
-	},
-	beforeScreenTransitionCalls: {
-
-	},
-	afterScreenTransitionCalls: {
-
-	}
+		render: function () {
+				$("body").prepend(this.$el);
+		},
+		events: {
+				"transitionend .screen-oldScreen, oTransitionEnd .screen-oldScreen, webkitTransitionEnd .screen-oldScreen": function () {
+						this.screens[0].unbind();
+						this.screens[0].remove();
+						this.screens.shift();
+						var afterScreenTransitionCall = this.afterScreenTransitionCalls[this.model.get("state") + "AfterCall"];
+						if (typeof afterScreenTransitionCall === "function") {
+								afterScreenTransitionCall();
+						}
+						$(".screen-newScreen").removeClass("screen-newScreen");
+						$("body").removeClass("is-screenTransition is-reverseTransition");
+				},
+				"click .back": function (event) {
+						event.preventDefault();
+						$("body").addClass("is-reverseTransition");
+						history.back();
+				}
+		},
+		createScreen: function () {
+				var newScreen = new ScreenView();
+				if (this.screens.length > 0) {
+						newScreen.$el.addClass("screen-newScreen");
+				}
+				this.screens.push(newScreen);
+				this.$el.append(newScreen.$el);
+		},
+		changeScreens: function () {
+				this.createScreen();
+				var beforeScreenTransitionCall = this.beforeScreenTransitionCalls[this.model.get("state") + "BeforeCall"];
+				if (typeof beforeScreenTransitionCall === "function") {
+						beforeScreenTransitionCall();
+				}
+				this.screens[0].$el.addClass("screen-oldScreen");
+				setTimeout(function () {
+						$("body").addClass("is-screenTransition");
+				}, 10);
+		},
+		beforeScreenTransitionCalls: {},
+		afterScreenTransitionCalls: {}
 });
-
 var appView;
-
-$(document).ready(function() {
-	appView = new AppView();
+$(document).ready(function () {
+		appView = new AppView();
 });
-
 var ScreenView = Backbone.View.extend({
-	tagName: "section",
-	className: "screen",
-	templates: {
-		"start": _.template($("#start").html()),
-	},
-	initialize: function() {
-		this.render();
-	},
-	render: function() {
-		this.$el.html(this.templates[app.get("state")]);
-	}
+		tagName: "section",
+		className: "screen",
+		templates: { "start": _.template($("#start").html()) },
+		initialize: function () {
+				this.render();
+		},
+		render: function () {
+				this.$el.html(this.templates[app.get("state")]);
+		}
 });
-
 var Router = Backbone.Router.extend({
-	routes: {
-		"": "start"
-	},
-	start: function() {
-		app.set("state", "start");
-	}
+		routes: { "": "start" },
+		start: function () {
+				app.set("state", "start");
+		}
 });
-
 var router = new Router();
-
 Backbone.history.start();
