7c57f0bf64d34c665168f92317f9e312e90d2136
uncleF <uncle.funkay@gmail.com>
2016-02-02 11:34:05 +0300

    Project Update

--- dev/res/js-dev/app.js
+++ dev/res/js-dev/app.js
@@ -2,4 +2,41 @@
 
 'use strict';
 
-(function() {})();
+(function() {
+
+  var Backbone = require('backbone');
+  var $ = require('jquery');
+  var _ = require('underscore');
+
+  var App = Backbone.Model.extend({
+    defaults: {
+      state: 'start'
+    }
+  });
+
+  var TodoRouter = Backbone.Router.extend({
+    routes: {
+      '': 'start'
+    },
+    start() {
+      app.set('state', 'start');
+    }
+  });
+
+  var app = new App();
+
+  app.TodoRouter = new TodoRouter();
+  Backbone.history.start();
+
+  app.AppView = Backbone.View.extend({
+    el: '#app',
+    events: {},
+    initialize() {
+      app.on('change:state', this.changeScreens, this);
+    },
+    createScreen() {}
+  });
+
+  new app.AppView();
+
+})();
